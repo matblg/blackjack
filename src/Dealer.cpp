d85af9986de6bfc163aa6c4a933908b277710c9e
Mathis Bélanger <mathisbelangerr@gmail.com>
2025-09-10 23:58:20 -0400

    dealer also has multi hand

--- src/Dealer.cpp
+++ src/Dealer.cpp
@@ -4,12 +4,12 @@ Dealer::Dealer() : Player("Dealer", 0) {}
 
 bool Dealer::shouldHit() const
 {
-    int value = hand.getValue();
+    int value = hands[0].getValue();
     if (value < 17)
         return true;
 
     // Hit soft 17
-    if (value == 17 && hand.hasAce())
+    if (value == 17 && hands[0].hasAce())
         return true;
 
     return false; // stand otherwise
